f83b44ad47b92e9166bf6e5645f68cd83c63cb5b
hackerkhan1212 <94687058+hackerkhan1212@users.noreply.github.com>
2022-10-16 13:00:50 +0500

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'Masukan Command nya', m)
 	await conn.reply(m.chat, mess.wait, 0, { thumbnail: await(await fetch(ext.thum)).buffer(), contextInfo: {
                   externalAdReply: {
-                    mediaUrl: 'https://youtu.be/-tKVN2mAKRI',
+                    mediaUrl: 'https://telegra.ph/file/232f4302a0240b8cd0d97.png',
                     title: ext.title,
                     body: ext.body,
                     thumbnail: await(await fetch(ext.thum)).buffer()
@@ -13,16 +13,17 @@ let handler = async(m, { conn, text }) => {
                  }
                }
            )
-	axios.get(`http://hujanapi.xyz/api/apkpure?query=${text}&apikey=qrQuAVo14XfmRIr`).then ((res) => {
+	axios.get(`https://api.lolhuman.xyz/api/playstore?apikey=85faf717d0545d14074659ad&query=${text}`).then ((res) => {
 	 	let hasil = `
-Nama App : ${res.data.result.data.title}
-Link download : ${res.data.result.data.link}
-Mau langsung download? Ketik .apkdl (link tersebut)`
+Name App : ${result.title}
+AppId : ${result.appId}		
+Link download : ${result.url}
+Want to download right away? Type .apkdl (AppId)`
 
     conn.reply(m.chat, hasil, m)
 	})
 }
-handler.help = ['apk (kata kunci)']
+handler.help = ['apk (keywords)']
 handler.tags = ['apk']
 handler.command = /^(apk)$/i
 handler.owner = false
